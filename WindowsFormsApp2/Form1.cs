d59088fb6ccd21e0bab545d97c906367386751a4
Elina Näslung Almlöf <elina.naslund.almlof@gmail.com>
2019-08-30 13:35:38 +0200

    Ny button

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
